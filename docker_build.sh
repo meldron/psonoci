8cf2a3485db5e6b4005ca976f3cc6b5d79ae0123
Bernd Kaiser <bk@dfjk.eu>
2020-06-17 17:07:12 +0200

    docker_build: use current user as container user and fix binary name

--- docker_build.sh
+++ docker_build.sh
@@ -5,6 +5,7 @@ set -e
 CACHE_DIR=docker_cache
 BUILD_DIR=build
 IMAGE_NAME=psonoci-builder
+BIN_NAME=psonoci
 
 cd "$(dirname "$0")"
 
@@ -17,10 +18,11 @@ mkdir -p "${BUILD_DIR}"
 sudo docker build . -t "$IMAGE_NAME"
 
 sudo docker run --rm -it \
+    --user "$(id -u):$(id -g)" \
     -v "$(pwd)/${CACHE_DIR}/git:/home/rust/.cargo/git" \
     -v "$(pwd)/${CACHE_DIR}/cargo-registry":/home/rust/.cargo/registry \
     -v "$(pwd)/${CACHE_DIR}/target:/home/rust/src/target" \
     "$IMAGE_NAME" cargo build --release
 
-cp "${CACHE_DIR}/target/x86_64-unknown-linux-musl/release/psoco" "${BUILD_DIR}"
-strip "${BUILD_DIR}/psoco"
+cp "${CACHE_DIR}/target/x86_64-unknown-linux-musl/release/${BIN_NAME}" "${BUILD_DIR}"
+strip "${BUILD_DIR}/${BIN_NAME}"
