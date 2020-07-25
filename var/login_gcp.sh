66c693d806052568c73a22e6c49eeb2a5853e696
Sascha Pfeiffer <sascha.pfeiffer@psono.com>
2020-07-25 08:06:46 +0200

    added deployment to get.psono.com
    Signed-off-by: Sascha Pfeiffer <sascha.pfeiffer@psono.com>

--- var/login_gcp.sh
+++ var/login_gcp.sh
@@ -6,6 +6,4 @@ echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | tee -a /e
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - && \
 apt-get update && apt-get -y install google-cloud-sdk && \
 echo "$GOOGLE_APPLICATION_CREDENTIALS" > "/root/key.json" && \
-gcloud auth activate-service-account --key-file=/root/key.json && \
-curl -H "PRIVATE-TOKEN: $GITLAB_PERSONAL_ACCESS_TOKEN" "https://gitlab.com/api/v4/projects/$CI_PROJECT_ID/repository/tags" --output changelog.json && \
-gsutil cp changelog.json gs://static.psono.com/gitlab.com/$CI_PROJECT_PATH/changelog.json
+gcloud auth activate-service-account --key-file=/root/key.json
