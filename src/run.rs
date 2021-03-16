c1400d64c8f755e387198cdd7d4cfe2ec1d97329
Bernd Kaiser <bk@dfjk.eu>
2021-03-16 17:34:14 +0100

    run: propagate exit codes

--- src/run.rs
+++ src/run.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
-// use std::ffi::OsString;
-use std::process::Command;
+use std::ffi::OsStr;
+use std::io::ErrorKind;
+use std::process::{exit, Command, ExitStatus};
 
 use anyhow::{Context, Result};
 use uuid::Uuid;
@@ -9,11 +10,15 @@ use crate::api::{api_key_get_secrets, Secret};
 use crate::config::Config;
 use crate::opt::RunCommand;
 
-fn create_env_vars_map(
+type EnvironmentVars = HashMap<String, String>;
+type Duplicates = HashMap<String, usize>;
+
+fn create_env_vars_map<'a>(
     secrets: HashMap<Uuid, Secret>,
     filter: Option<Vec<Uuid>>,
-) -> HashMap<String, String> {
+) -> (HashMap<String, String>, Duplicates) {
     let mut env_vars_map: HashMap<String, String> = HashMap::new();
+    let mut duplicates: Duplicates = HashMap::new();
 
     let filtered: HashMap<Uuid, Secret> = match filter {
         Some(ids) => secrets
@@ -31,17 +36,93 @@ fn create_env_vars_map(
             if let Some(evs) = s.env_vars {
                 evs.into_iter().for_each(|ev| {
                     if env_vars_map.contains_key(&ev.key) {
-                        eprintln!(
-                            "psonoci warning: duplicate env var with name {}; previous one will be overwritten",
-                            &ev.key
-                        );
+                        let key = ev.key.clone();
+                        *duplicates.entry(key).or_insert(0) += 1;
                     }
                     env_vars_map.insert(ev.key, ev.value);
                 });
             }
         });
 
-    env_vars_map
+    (env_vars_map, duplicates)
+}
+
+fn build_command<P, A>(
+    program: P,
+    args: A,
+    envs: &EnvironmentVars,
+    duplicates: &Duplicates,
+    clear_env: bool,
+) -> Command
+where
+    P: AsRef<OsStr>,
+    A: IntoIterator,
+    A::Item: AsRef<OsStr>,
+{
+    let mut command = Command::new(program);
+    command.args(args);
+
+    if clear_env {
+        command.env_clear();
+    }
+
+    if envs.len() > 0 {
+        duplicates.into_iter().for_each(|(name, dup)| {
+            eprintln!(
+                "psonoci warning: duplicate env var with name '{}'; overwritten '{}' times",
+                name, dup
+            );
+        });
+        command.envs(envs);
+    } else {
+        eprintln!("psonoci warning: no env vars found");
+    }
+
+    command
+}
+
+#[cfg(target_family = "unix")]
+fn execute_error_status_code(error_kind: &ErrorKind) -> i32 {
+    // this emulates the behavior of shells like bash and zsh
+    // https://tldp.org/LDP/abs/html/exitcodes.html
+    match error_kind {
+        ErrorKind::PermissionDenied => 126,
+        ErrorKind::NotFound => 127,
+        _ => 1,
+    }
+}
+
+#[cfg(not(target_family = "unix"))]
+fn execute_error_status_code(error_kind: &ErrorKind) -> i32 {
+    1
+}
+
+#[cfg(target_family = "unix")]
+fn exit_status_code(exit_status: &ExitStatus) -> i32 {
+    use std::os::unix::prelude::*;
+
+    match exit_status.code() {
+        Some(code) => code,
+        None => {
+            let signal: Option<i32> = exit_status.signal();
+
+            // this emulates the behavior of shells like bash and zsh
+            // 128+n fatal error signal "n"
+            // https://tldp.org/LDP/abs/html/exitcodes.html
+            match signal {
+                Some(n) => 128 + n,
+                None => 1,
+            }
+        }
+    }
+}
+
+#[cfg(not(target_family = "unix"))]
+fn exit_status_code(exit_status: &ExitStatus) -> i32 {
+    match exit_status.code() {
+        Some(code) => code,
+        None => 1,
+    }
 }
 
 pub fn run_run_command(config: Config, rc: RunCommand) -> Result<()> {
@@ -50,24 +131,26 @@ pub fn run_run_command(config: Config, rc: RunCommand) -> Result<()> {
     let args = &command_values[1..];
 
     let secrets = api_key_get_secrets(&config).context("api key secrets failed")?;
-    let env_vars = create_env_vars_map(secrets, rc.filter);
+    let (envs, duplicates) = create_env_vars_map(secrets, rc.filter);
 
-    let mut command = Command::new(program);
-    command.args(args);
+    let mut command = build_command(program, args, &envs, &duplicates, rc.clear_env);
 
-    if rc.clear_env {
-        command.env_clear();
+    let command_result = command.status();
+
+    if let Err(e) = &command_result {
+        let error_kind = e.kind();
+
+        let spawn_error_code = execute_error_status_code(&error_kind);
+
+        eprintln!("Could not spawn {:?}: {}", program, e);
+
+        exit(spawn_error_code);
     }
 
-    if env_vars.len() > 0 {
-        command.envs(env_vars);
-    } else {
-        eprintln!("psonoci warning: no env vars found");
-    }
+    let exit_status =
+        command_result.expect("exit was error; should not happen please file a bug report");
 
-    command
-        .status()
-        .context(format!("spawning program {:?} failed", program))?;
+    let exit_status_code = exit_status_code(&exit_status);
 
-    Ok(())
+    exit(exit_status_code)
 }
