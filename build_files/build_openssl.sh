74007ed2744fced7160ceb931989acfdbd435efa
Bernd Kaiser <bk@dfjk.eu>
2021-11-21 23:09:28 +0100

    Upgrade openssl to 1.1.1l

--- build_files/build_openssl.sh
+++ build_files/build_openssl.sh
@@ -4,7 +4,7 @@
 set -ex
 
 main() {
-    local version=1.1.1k
+    local version=1.1.1l
     local os=$1 \
           triple=$2
 
@@ -46,7 +46,7 @@ main() {
       -fPIC $disable_secure_memory \
       "${@:3}"
     nice make -j"$(nproc)"
-    make install
+    make install_sw
 
     # clean up
     apt-get purge --auto-remove -y "${purge_list[@]}"
