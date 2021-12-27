69a6e0e1e53579515d00e2183f0d527eaee22659
Bernd Kaiser <bk@dfjk.eu>
2021-12-27 13:49:10 +0100

    Build cross images from scratch
    cross images for aarch64-unknown-linux-musl and
    armv7-unknown-linux-gnueabihf are buggy, so this
    commit adds files to build them from scratch

--- build_files/build_cross_linux_images_with_openssl.sh
+++ build_files/build_cross_linux_images_with_openssl.sh
@@ -4,14 +4,13 @@ set -eu
 
 TARGETS=(
     "aarch64-unknown-linux-musl"
-    "armv7-unknown-linux-musleabihf"
     "x86_64-unknown-linux-musl"
     "armv7-unknown-linux-gnueabihf"
 )
 
 cd "$(dirname "$0")"
 
-for target in ${TARGETS[*]}; do
+for target in "${TARGETS[@]}"; do
     image_name="meldron/psonoci_builder:$target"
     echo "Building ${image_name}"
     docker build --pull -f "Dockerfile-$target" -t "$image_name" .
